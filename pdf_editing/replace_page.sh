6cc0934cb751386a967a9fba3c2d36bab2af5d35
Andrew Garcia <garcia.gtr@gmail.com>
2023-09-15 00:58:48 -0500

    can now replace ANY book.pdf page by page.pdf

--- pdf_editing/replace_page.sh
+++ pdf_editing/replace_page.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ######################################################################################
 # Script: replace_page.sh                                                       
 # Description: This script is designed to replace a page in a PDF file with a       
@@ -9,8 +10,8 @@
 # Date: Sep 13 2023                                                   
 #                                                                                 
 # Usage:                                                                          
-#   ./replace_page.sh <book.pdf> <page.pdf> <page_number>                   
-#                                                                           
+#   ./replace_page.sh <book.pdf> <page.pdf> <page_number>
+#                                                                             
 ######################################################################################
 
 # Check for the correct number of arguments
@@ -42,27 +43,31 @@ front_pdf="__TEMP__FRONT.pdf"
 frontagg_pdf="__TEMP__FRONTAGG.pdf"
 back_pdf="__TEMP__BACK.pdf"
 
-# Extract pages from the beginning of book.pdf up to (page_number - 1)
-qpdf "$book_pdf" --pages . 1-$((page_number - 1)) -- "$front_pdf"
-
-# Concatenate extracted pages with page.pdf
-qpdf "$front_pdf" --pages "$front_pdf" 1-$((page_number - 1)) "$page_pdf" 1-z -- "$frontagg_pdf"
-
-if ! [ "$page_number" -eq "$(qpdf "$book_pdf" --show-npages)" ]; then
-    # Extract pages from (page_number + 1) to the end of book.pdf
-    qpdf "$book_pdf" --pages . $((page_number + 1))-z -- "$back_pdf"
-
-    # Concatenate frontagg_pdf and back_pdf to create the final output PDF
-    qpdf "$frontagg_pdf" --pages "$frontagg_pdf" 1-z "$back_pdf" 1-z -- "$output_pdf"
+if [ "$page_number" -eq 1 ]; then
+    # If replacing the first page, simply concatenate page.pdf with the rest of book.pdf
+    qpdf "$page_pdf" --pages . "$book_pdf" 2-z -- "$output_pdf"
 else
-    cp "$frontagg_pdf" "$output_pdf"
-    touch "$back_pdf"
+    # Extract pages from the beginning of book.pdf up to (page_number - 1)
+    qpdf "$book_pdf" --pages . 1-$((page_number - 1)) -- "$front_pdf"
+
+    # Concatenate extracted pages with page.pdf
+    qpdf "$front_pdf" --pages "$front_pdf" 1-$((page_number - 1)) "$page_pdf" 1-z -- "$frontagg_pdf"
+
+    if ! [ "$page_number" -eq "$(qpdf "$book_pdf" --show-npages)" ]; then
+        # Extract pages from (page_number + 1) to the end of book.pdf
+        qpdf "$book_pdf" --pages . $((page_number + 1))-z -- "$back_pdf"
+
+        # Concatenate frontagg_pdf and back_pdf to create the final output PDF
+        qpdf "$frontagg_pdf" --pages "$frontagg_pdf" 1-z "$back_pdf" 1-z -- "$output_pdf"
+    else
+        cp "$frontagg_pdf" "$output_pdf"
+        touch "$back_pdf"
+    fi
+
+    # Clean up intermediate files
+    rm "$front_pdf"
+    rm "$frontagg_pdf"
+    rm "$back_pdf"
 fi
-
-
-# Clean up intermediate files
-rm "$front_pdf"
-rm "$frontagg_pdf"
-rm "$back_pdf"
 
 echo "Page $page_number of $book_pdf has been replaced with $page_pdf in $output_pdf."
