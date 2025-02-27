46ae36f28da450594d016323fa42bf1df465ee88
Andrew Garcia <garcia.gtr@gmail.com>
2025-02-27 14:25:58 -0500

    If replacing the first page, simply concatenate page.pdf with the rest of book.pdf

--- pdf_editing/replace_page.sh
+++ pdf_editing/replace_page.sh
@@ -43,9 +43,14 @@ front_pdf="__TEMP__FRONT.pdf"
 frontagg_pdf="__TEMP__FRONTAGG.pdf"
 back_pdf="__TEMP__BACK.pdf"
 
-if [ "$page_number" -eq 1 ]; then
+if [ "$page_number" -eq 0 ]; then
+    # If replacing the first page, simply concatenate page.pdf with the rest of book.pdf
+    qpdf "$book_pdf" --pages "$page_pdf" 1-z "$book_pdf" 1-z -- "$output_pdf"
+    
+elif [ "$page_number" -eq 1 ]; then
     # If replacing the first page, simply concatenate page.pdf with the rest of book.pdf
     qpdf "$page_pdf" --pages . "$book_pdf" 2-z -- "$output_pdf"
+
 else
     # Extract pages from the beginning of book.pdf up to (page_number - 1)
     qpdf "$book_pdf" --pages . 1-$((page_number - 1)) -- "$front_pdf"
